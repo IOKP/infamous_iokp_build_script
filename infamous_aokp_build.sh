43c2589a8f8a3cd89b2ed7d6da2d652d39e6e43c
mrimp <mrimp@day-off.net>
2013-10-01 10:31:27 -0400

    Updated to MR2

--- infamous_aokp_build.sh
+++ infamous_aokp_build.sh
@@ -5,7 +5,7 @@ cd ~/android/aokp_jb
 echo "Done"
 echo "Initialize the AOKP repo and download the manifest"
 sleep 2
-repo init -u https://github.com/AOKP/platform_manifest.git -b jb-mr1
+repo init -u https://github.com/AOKP/platform_manifest.git -b jb-mr2
 echo "Done"
 echo "Sync AOKP source"
 echo "Sit and wait for a long time. Nearing 10GB of source code now."
